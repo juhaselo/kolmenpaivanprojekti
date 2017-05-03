48442f401f0be383926fa56d5ab0656037a8db87
Juha Selonen <juha.selonen@outlook.com>
2017-05-03 10:52:38 +0300

    Vaihe6 ja 7

--- Vaihe6ja7/main.js
+++ Vaihe6ja7/main.js
@@ -97,6 +97,75 @@ function processJson(data) {
 
 readDatabase();
 
+function print(result) {
+    for(var e of result) {
+        //console.log(e.name+': '+e.music+', '+e.tv);
+        console.log(e.name);
+        console.log('------');
+    }
+
+}
+
+// exact match
+function fieldMatch(entry, fieldName, value) {
+    if(value) {
+     value = value.toLowerCase();
+     if(entry[fieldName].toLowerCase() != value) { // ! -> negation
+        return false;
+     }
+   }
+   return true; // if value is not set or matches, return true
+}
+
+// contains substring
+function fieldIncludes(entry, fieldName, value) {
+   if(value) {
+     value = value.toLowerCase();
+     if(!entry[fieldName].toLowerCase().includes(value)) {
+        return false; // match, return true
+     }
+   }
+   return true; // if value is not set or matches, return true
+}
+// music, restaurants, etc
+function fieldArrayContains(entry, fieldName, value) {
+   var match = false;
+   if(value) {
+     if(!entry[fieldName]) { // if field undefined, return false
+       return false;
+     }
+     value = value.toLowerCase();
+     for(var dbValue of entry[fieldName]) {
+       if(dbValue.toLowerCase().includes(value)) {
+         match = true;
+       }
+     }
+     return match;
+   }
+   return true; // if value is not defined, return true
+}
+
+function includeEntry(entry, person, gender, music, tv, movies, restaurants) {
+   if(!fieldIncludes(entry, 'name', person)) {
+     return false;
+   }
+   if(!fieldMatch(entry, 'gender', gender)) {
+     return false;
+   }
+   if(!fieldArrayContains(entry, 'music', music)) {
+     return false;
+   }
+   if(!fieldArrayContains(entry, 'tv', tv)) {
+     return false;
+   }
+   if(!fieldArrayContains(entry, 'movies', movies)) {
+     return false;
+   }
+   if(!fieldArrayContains(entry, 'restaurants', restaurants)) {
+     return false;
+   }
+   return true;
+}
 function search(person, gender, music, tv, movies, restaurants) {
    console.log('search');
    var result = [];
@@ -108,14 +177,6 @@ function search(person, gender, music, tv, movies, restaurants) {
    return result;
 }
 
-function print(result) {
-    for(var e of result) {
-        //console.log(e.name+': '+e.music+', '+e.tv);
-        console.log(e.name);
-        console.log('------');
-    }
-
-}
 print(search('', '', 'Haloo Helsinki', '', '', ''));
 
 
